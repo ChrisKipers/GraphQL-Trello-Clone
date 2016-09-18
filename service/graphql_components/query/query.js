160130b9c61bc09bd19c2cb20baab52d91329ffa
Chris Kipers <ckipers@gmail.com>
2016-09-18 18:53:30 -0400

    udpated query for get board by id

--- service/graphql_components/query/query.js
+++ service/graphql_components/query/query.js
@@ -1,31 +1,28 @@
-const {GraphQLObjectType, GraphQLString, GraphQLList, GraphQLID} = require('graphql');
+const {GraphQLObjectType, GraphQLString, GraphQLList, GraphQLID, GraphQLNonNull} = require('graphql');
 
-const {userDao} = require('../../dao/user_dao');
-const {userTransformer} = require('../../transformers/user_transformer');
 const {boardDao} = require('../../dao/board_dao');
 const {boardTransformer} = require('../../transformers/board_transformer');
 const {boardGraphQLType} = require('./board');
-const {userGraphQLType} = require('./user');
 
 const query = new GraphQLObjectType({
   name: 'Query',
   fields: () => ({
-    users: {
-      type: new GraphQLList(userGraphQLType),
-      resolve() {
-        return userDao.findAll().then(users => users.map(userTransformer.transform));
-      }
-    },
-    user: {
-      type: userGraphQLType,
-      resolve() {
-        return userDao.findAll().then(users => userTransformer.transform(users[0]));
-      }
-    },
     boards: {
       type: new GraphQLList(boardGraphQLType),
       resolve() {
         return boardDao.findAll().then(boards => boards.map(boardTransformer.transform));
+      }
+    },
+    board: {
+      type: boardGraphQLType,
+      args: {
+        id: {
+          name: 'id',
+          type: new GraphQLNonNull(GraphQLID)
+        }
+      },
+      resolve(_, {id}) {
+        return boardDao.findOne({id}).then(boardTransformer.transform);
       }
     }
   })
