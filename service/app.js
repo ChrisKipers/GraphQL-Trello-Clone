940dd8468e88d99e80de45de0809b3c9a43155a9
Chris Kipers <ckipers@gmail.com>
2016-09-18 15:02:19 -0400

    added create board input

--- service/app.js
+++ service/app.js
@@ -1,33 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
 
 const {connection} = require('./dao/connection');
 const {schema} = require('./graphql_components/schema');
 
+
 connection.sync();
 
 const app = express();
 
-app.use((req, res, next) => {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-
-});
-
-app.use('/graphql', graphqlHTTP({
+app.use('/graphql', cors(), graphqlHTTP({
   schema: schema,
   graphiql: true
 }));
